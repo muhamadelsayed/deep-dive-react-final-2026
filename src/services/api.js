083c27c2c62017bdc83f30a36a7a462f92e46602
muhammed sleem <masalim122006@gmail.com>
2026-09-21 21:30:37 +0300

    add ssl support

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://final-prject-deep-dive-zageng.runasp.net",
+    "https://final-prject-deep-dive-zageng.runasp.net",
 
   headers: {
     "Content-Type": "application/json",
